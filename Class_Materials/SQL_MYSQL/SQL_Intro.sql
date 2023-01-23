fa89769682aaeef73d526d62f2a209c1d3254bad
poppynevinadley <nevin.poppy@outlook.com>
2023-01-23 15:19:36 +0100

    Update SQL_Intro.sql

--- Class_Materials/SQL_MYSQL/SQL_Intro.sql
+++ Class_Materials/SQL_MYSQL/SQL_Intro.sql
@@ -1,8 +1,9 @@
 
 use bank;
-select  distinct ( account_id), district_id,frequency,date  from bank.account
+select  distinct ( account_id), district_id,frequency,date  from bank.account #means unique
 
-order by district_id desc;
+order by district_id desc; 
+#select *, CASE WHEN operation = 'VKLAD' THEN 'YES' ELSE 'NO' END AS new_op FROM trans;
 
 
 select district_id,count(account_id) from bank.account group by district_id;
@@ -11,12 +12,14 @@ select district_id,count(account_id) from bank.account group by district_id;
 select account_id + 5 AS updated_account_id  from bank.account;
 
 
-select * from account ;
+select * from account ; #xtrim is the same as strip in python where you remove white spaces unless spec, lstrip, rstrip, ltrim, rtrim, LEFT = left(col,n) - from left 
+#ltrim - removing all leading blank spaces from the left, 
+#substring, grab parts of the string, 
 
-select *,amount- payments AS updated_payments from bank.loan;
+select *,amount- payments AS updated_payments from bank.loan; #floor(avg(amount)) - rounds down, ceiling rounds up or can use round
 
 SELECT * FROM bank.loan
-WHERE status NOT IN ("B","b") AND amount > 100000;
+WHERE status NOT IN ("B","b") AND amount > 100000; #<> means different than, can put inbetween instead of NOT IN, inbetween is
 
 
 SELECT distinct A3  FROM bank.district;
